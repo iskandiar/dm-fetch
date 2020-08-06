8a14fc1948a28bc85d1f266b09eead6d896d3c31
Aleksander Maj <aaarys@gmail.com>
2020-08-06 16:56:48 +0200

    Init repo

---
+++ index.js
@@ -0,0 +1,36 @@
+import html2canvas from 'html2canvas'
+
+const dmFetch = (url, participantKey, options = { tracking: true }) => {
+  const trackingURL = `${url}/tracking`
+  const _fetch = fetch
+
+  window.fetch = function () {
+    const content = document.body.innerHTML
+    const lastTrackingTimestamp = localStorage.getItem('lastTrackingTimestamp')
+
+    const args = arguments
+
+    args[0] = `${url}${args[0]}?key=${participantKey}`
+
+    if (lastTrackingTimestamp < Date.now() - 120 * 1000 && options.tracking) {
+      localStorage.setItem('lastTrackingTimestamp', Date.now())
+  
+      html2canvas(document.body).then(canvas => {
+        _fetch(trackingURL, {
+          method: 'POST',
+          body: JSON.stringify({
+            preview: canvas.toDataURL(),
+            content,
+            participantKey
+          })
+        })
+      });
+    }
+
+    window.nativeFetch = _fetch
+
+    return Promise.resolve(_fetch.apply(window, args))
+  }
+}
+
+export default dmFetch
