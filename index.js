7a491acc746f773857eccce9a6b830a9202e265a
Aleksander Maj <aaarys@gmail.com>
2020-08-13 13:05:40 +0200

    [1.0.1] Fix export statement

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import configureLiveApi from './src/fetch-monkeypatch'
 import configureOfflineApi from './src/offline-api-mock'
 
-export default ({ configureLiveApi, configureOfflineApi })
+export { configureLiveApi, configureOfflineApi }
